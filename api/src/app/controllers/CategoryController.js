c40968b851f959017ffab36f192f3c549c9b1e6c
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-04-11 11:01:58 -0300

    mycontacts backend

---
+++ api/src/app/controllers/CategoryController.js
@@ -0,0 +1,61 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const categories = await CategoriesRepository.findAll();
+
+    response.json(categories);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await CategoriesRepository.findById(id);
+
+    if (!category) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(category);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    response.json(category);
+  }
+
+  async update(request, response) {
+    const { name } = request.body;
+    const { id } = request.params;
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    const category = await CategoriesRepository.update(name, id);
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoriesRepository.delete(id);
+    response.sendStatus(204);
+  }
+}
+
+module.exports = new CategoryController();
---
+++ api/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('express-async-errors');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json()); // Middleware que permite que a API receba requisições com o body
+app.use(routes); // Express entende as rotas como Middlewares, por isso 'app.use'
+app.use((error, request, response, next) => {
+  console.log('Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
+
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
