107b4b53e487894585f13e6a06b13c4f8dc5bf39
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-04-12 12:20:39 -0300

    create Card of ContactList

--- fe/src/components/ContactsList/style.js
+++ fe/src/components/ContactsList/style.js
@@ -10,7 +10,6 @@ export const Header = styled.header`
   align-items: center;
 
   strong {
-    color: #222;
     font-size: 24px;
   }
 
@@ -26,6 +25,77 @@ export const Header = styled.header`
     &:hover {
       background: ${({ theme }) => theme.colors.primary.main};
       color: #fff;
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 8px;
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+
+      span {
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: bold;
+        font-size: 16px;
+
+        margin-right: 8px;
+      }
+  }
+  }
+`;
+
+export const Card = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  padding: 16px;
+  border-radius: 4px;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: bold;
+        text-transform: uppercase;
+        border-radius: 4px;
+        margin-left: 8px;
+        padding: 4px;
+      }
+    }
+
+    span {
+      display: block;
+      color: ${({ theme }) => theme.colors.gray[200]};
+      font-size: 14px;
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 8px;
     }
   }
 `;
