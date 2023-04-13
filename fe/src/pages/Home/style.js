7dc36db3f8b94d61fdcc582894951c8f80d1e13d
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-04-13 09:35:22 -0300

    add style for Home

---
+++ fe/src/pages/Home/style.js
@@ -0,0 +1,123 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 32px;
+`;
+
+export const InputSearchContainer = styled.div`
+  width: 100%;
+
+  input {
+    width: 100%;
+    background: #fff;
+    border: none;
+    border-radius: 25px;
+    height: 50px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    outline: 0;
+    padding: 0 16px;
+    font-size: 16px;
+
+    &::placeholder {
+      color: #BCBCBC;
+    }
+  }
+
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 32px;
+
+  strong {
+    font-size: 24px;
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.primary.main};
+    text-decoration: none;
+    font-weight: bold;
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+    border-radius: 4px;
+    padding: 8px 16px;
+    transition: all 0.2s ease-in;
+
+    &:hover {
+      background: ${({ theme }) => theme.colors.primary.main};
+      color: #fff;
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 8px;
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+
+      span {
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: bold;
+        font-size: 16px;
+
+        margin-right: 8px;
+      }
+  }
+  }
+`;
+
+export const Card = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  padding: 16px;
+  border-radius: 4px;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: bold;
+        text-transform: uppercase;
+        border-radius: 4px;
+        margin-left: 8px;
+        padding: 4px;
+      }
+    }
+
+    span {
+      display: block;
+      color: ${({ theme }) => theme.colors.gray[200]};
+      font-size: 14px;
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 8px;
+    }
+  }
+`;
