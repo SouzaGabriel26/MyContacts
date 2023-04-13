74e04d5a5acc19bbcc819158b1bfba79e92ef92f
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-04-13 09:20:30 -0300

    setting react router

--- fe/src/components/Header/styles.js
+++ fe/src/components/Header/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-  margin-top: 66px;
+  margin-top: 70px;
   display: flex;
   flex-direction: column;
   justify-content: center;
