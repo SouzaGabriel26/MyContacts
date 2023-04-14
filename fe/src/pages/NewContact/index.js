fc483517c2b0d88d5bb55441c11169c42db5e75d
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-04-14 10:00:49 -0300

    add ContactForm with the buttonLabel

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,31 +1,13 @@
 import PageHeader from '../../components/PageHeader';
 
-import Input from '../../components/Input';
-import Select from '../../components/Select';
-import Button from '../../components/Button';
+import ContactForm from '../../components/ContactForm';
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo contato" />
 
-      <Input type="text" placeholder="Nome" />
-      <Input type="email" placeholder="Email" />
-      <Input type="number" placeholder="Telefone" />
-
-      <Select>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-      </Select>
-
-      <Button type="button">
-        Salvar alterações
-      </Button>
-      <Button type="button" disabled>
-        Salvar alterações
-      </Button>
+      <ContactForm buttonLabel="Cadastrar" />
     </>
   );
 }
