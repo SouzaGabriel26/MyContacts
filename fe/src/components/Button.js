59794e4903600a2c263ef1f3bce6ff9c08cf8ea2
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-04-14 10:53:07 -0300

    width refactor

--- fe/src/components/Button.js
+++ fe/src/components/Button.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 export default styled.button`
-  width: 100%;
   height: 52px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   border-radius: 4px;
+  padding: 0 16px;
   background-color: ${({ theme }) => theme.colors.primary.main};
   color: #fff;
 
