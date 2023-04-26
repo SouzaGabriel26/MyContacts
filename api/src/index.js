a82ba242039b130f5df53b6c13c25111707ee0cb
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-04-26 15:14:02 -0300

    using cors middleware to allow an origin

--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json()); // Middleware que permite que a API receba requisições com o body
+app.use(cors);
 app.use(routes); // Express entende as rotas como Middlewares, por isso 'app.use'
-app.use((error, request, response, next) => {
-  console.log('Error Handler');
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3001, () => console.log('Server started at http://localhost:3001'));
