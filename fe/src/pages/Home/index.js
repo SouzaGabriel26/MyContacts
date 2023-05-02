8a6a56d34bcbb7a6ca04bd4cc541bb692daaf8cb
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-05-02 16:35:38 -0300

    refactor: use async/await

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -24,21 +24,23 @@ export default function Home() {
   )), [contacts, searchTerm]);
 
   useEffect(() => {
-    setIsLoading(true);
+    async function lodContacts() {
+      try {
+        setIsLoading(true);
 
-    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
-      .then(async (response) => {
+        const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
         await delay(500);
 
         const json = await response.json();
         setContacts(json);
-      })
-      .catch((error) => {
-        console.log('erro', error);
-      })
-      .finally(() => {
+      } catch (error) {
+        console.log('error', error);
+      } finally {
         setIsLoading(false);
-      });
+      }
+    }
+
+    lodContacts();
   }, [orderBy]);
 
   function handleToggleOrderBy() {
