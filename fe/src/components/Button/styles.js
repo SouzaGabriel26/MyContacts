ea5df0b62e932a1688eee4f8d45d28489804cbb5
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-05-20 20:20:05 -0300

    Componentizing Button

--- fe/src/components/Button/styles.js
+++ fe/src/components/Button/styles.js
@@ -1,18 +1,19 @@
 import styled, { css } from 'styled-components';
 
-export default styled.button`
+export const StyledButton = styled.button`
   height: 52px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   border-radius: 4px;
   padding: 0 16px;
   background-color: ${({ theme }) => theme.colors.primary.main};
   color: #fff;
-
   border: none;
   font-size: 16px;
   font-weight: bold;
-
   transition: background 0.2s ease-in;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   &:hover {
     background: ${({ theme }) => theme.colors.primary.light};
