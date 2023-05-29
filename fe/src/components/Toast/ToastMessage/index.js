ac3e6fe54bf4a6f8e6f18cd3c775247ee2870674
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-05-29 11:57:22 -0300

    cleaning the toast component after a period of time

--- fe/src/components/Toast/ToastMessage/index.js
+++ fe/src/components/Toast/ToastMessage/index.js
@@ -1,10 +1,21 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 
 export default function ToastMessage({ message, onRemoveMessage }) {
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      onRemoveMessage(message.id);
+    }, message.duration || 7000);
+
+    return () => {
+      clearTimeout(timeOutId);
+    };
+  }, [message, onRemoveMessage]);
+
   function handleRemoveToast() {
     onRemoveMessage(message.id);
   }
@@ -30,6 +41,7 @@ ToastMessage.propTypes = {
     id: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
     type: PropTypes.oneOf(['default', 'danger', 'success']),
+    duration: PropTypes.number,
   }).isRequired,
   onRemoveMessage: PropTypes.func.isRequired,
 };
