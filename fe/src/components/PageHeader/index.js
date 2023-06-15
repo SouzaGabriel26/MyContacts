d6daf7614428bdd2a03f2e195815904e4598bf9b
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-06-15 17:49:47 -0300

    refactor: add pathToReturn props

--- fe/src/components/PageHeader/index.js
+++ fe/src/components/PageHeader/index.js
@@ -5,10 +5,10 @@ import arrow from '../../assets/images/icons/arrow.svg';
 
 import { Container } from './styles';
 
-export default function PageHeader({ title }) {
+export default function PageHeader({ title, pathToReturn }) {
   return (
     <Container>
-      <Link to="/">
+      <Link to={pathToReturn}>
         <img src={arrow} alt="Back" />
         <span>Voltar</span>
       </Link>
@@ -19,4 +19,9 @@ export default function PageHeader({ title }) {
 
 PageHeader.propTypes = {
   title: PropTypes.string.isRequired,
+  pathToReturn: PropTypes.string,
+};
+
+PageHeader.defaultProps = {
+  pathToReturn: '/',
 };
