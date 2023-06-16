e1ae8463db28c89c7457703ecc31ab1bc38db9a2
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-06-16 11:19:45 -0300

    refactor editContact path and add editCategory

--- fe/src/Routes.js
+++ fe/src/Routes.js
@@ -4,14 +4,16 @@ import Home from './pages/Home';
 import NewContact from './pages/NewContact';
 import EditContact from './pages/EditContact';
 import NewCategory from './pages/NewCategory';
+import EditCategory from './pages/EditCategory';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/newContact" component={NewContact} />
-      <Route path="/edit/:id" component={EditContact} />
+      <Route path="/editContact/:id" component={EditContact} />
       <Route path="/newCategory" component={NewCategory} />
+      <Route path="/editCategory/:id" component={EditCategory} />
     </Switch>
   );
 }
