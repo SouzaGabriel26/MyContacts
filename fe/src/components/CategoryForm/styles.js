4f82483d7461f4c2abd51f7d4d74f48745391ce4
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-06-16 11:20:32 -0300

    categoryForm styles

---
+++ fe/src/components/CategoryForm/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const CategoriesForm = styled.form``;
+
+export const ButtonContainer = styled.div`
+
+  margin: 24px 0px;
+
+  button {
+    width: 100%;
+  }
+`;
+
+export const FormGroup = styled.div`
+  position: relative;
+  display: flex;
+
+  div {
+    position: absolute;
+    right: 16px;
+    top: 18px;
+  }
+`;
