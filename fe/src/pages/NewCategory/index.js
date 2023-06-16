8dc500a205e5ad66ccb9b1c08ee150b69d57dfeb
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-06-16 11:22:51 -0300

    refactor: Using CategoryForm component

--- fe/src/pages/NewCategory/index.js
+++ fe/src/pages/NewCategory/index.js
@@ -1,20 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import PageHeader from '../../components/PageHeader';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import trash from '../../assets/images/icons/trash.svg';
+import edit from '../../assets/images/icons/edit.svg';
+
 import CategoriesService from '../../services/CategoriesService';
 import toast from '../../utils/toast';
 
+import Modal from '../../components/Modal';
+import PageHeader from '../../components/PageHeader';
 import Loader from '../../components/Loader';
-import Input from '../../components/Input';
 
-import { ListCategories, CategoriesForm, ButtonContainer } from './styles';
-import Button from '../../components/Button';
+import {
+  TitleLabel,
+  CategoryCard,
+  CategoriesList,
+} from './styles';
+import CategoryForm from '../../components/CategoryForm';
 
 function NewCategory() {
   const [categories, setCategories] = useState();
   const [isLoadingCategories, setIsLoadingCategories] = useState(true);
-  const [categoryName, setCategoryName] = useState('');
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const [isLoadingDelete, setIsLoadingDelete] = useState(false);
+  const [categoryBeingDeleted, setCategoryBeingDeleted] = useState();
 
-  const isCategoryNameInputInvalid = categoryName === '';
+  const categoryFormRef = useRef(null);
 
   useEffect(() => {
     async function loadCategories() {
@@ -36,9 +47,7 @@ function NewCategory() {
     loadCategories();
   }, []);
 
-  async function handleCategorySubmit(e) {
-    e.preventDefault();
-
+  async function handleCategorySubmit({ categoryName }) {
     try {
       const categoryObj = {
         name: categoryName,
@@ -46,7 +55,9 @@ function NewCategory() {
 
       const newCategory = await CategoriesService.createCategory(categoryObj);
 
-      setCategories((prevState) => ({ ...prevState, newCategory }));
+      categoryFormRef.current.resetCategoryField();
+
+      setCategories((prevState) => ([...prevState, newCategory]));
 
       toast({
         type: 'success',
@@ -57,8 +68,42 @@ function NewCategory() {
         type: 'danger',
         text: 'Erro ao cadastrar a categoria',
       });
+    }
+  }
+
+  function handleDeleteCategory(category) {
+    setIsDeleteModalVisible(true);
+    setCategoryBeingDeleted(category);
+  }
+
+  function handleCloseDeleteModal() {
+    setIsDeleteModalVisible(false);
+    setCategoryBeingDeleted(null);
+  }
+
+  async function handleConfirmDeleteContact() {
+    try {
+      setIsLoadingDelete(true);
+
+      await CategoriesService.deleteCategory(categoryBeingDeleted.id);
+
+      setCategories((prevState) => prevState.filter(
+        (category) => category.id !== categoryBeingDeleted.id,
+      ));
+
+      handleCloseDeleteModal();
+
+      toast({
+        type: 'success',
+        text: 'Categoria deletada com sucesso!',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao deletar a categoria!',
+      });
     } finally {
-      setCategoryName('');
+      setIsLoadingDelete(false);
     }
   }
 
@@ -67,41 +112,61 @@ function NewCategory() {
 
       <Loader isLoading={isLoadingCategories} />
 
+      <Modal
+        title={`Tem certeza que deseja remover o contato "${categoryBeingDeleted?.name}"?`}
+        visible={isDeleteModalVisible}
+        isLoading={isLoadingDelete}
+        danger
+        confirmLabel="Deletar"
+        onCancel={handleCloseDeleteModal}
+        onConfirm={handleConfirmDeleteContact}
+      >
+        <p>Esta ação não poderá ser desfeita!</p>
+      </Modal>
+
       <PageHeader title="Nova categoria" pathToReturn="/newContact" />
 
-      <CategoriesForm onSubmit={handleCategorySubmit}>
-        <Input
-          value={categoryName}
-          required
-          placeholder="Digite o nome da categoria"
-          onChange={(e) => setCategoryName(e.target.value)}
-        />
+      <CategoryForm
+        ref={categoryFormRef}
+        buttonLabel="Cadastrar"
+        onSubmit={handleCategorySubmit}
+        isLoading={isLoadingCategories}
+      />
 
-        <ButtonContainer>
-          <Button
-            isLoading={isLoadingCategories}
-            disabled={isCategoryNameInputInvalid}
-            type="submit"
-          >
-            Cadastrar
-          </Button>
-        </ButtonContainer>
-      </CategoriesForm>
+      {!isLoadingCategories && (
+      <TitleLabel>
+        Lista de Categorias (
+          {categories.length > 0 ? categories.length : null}
+        ):
+      </TitleLabel>
+      )}
 
-      <ListCategories>
-
-        {!isLoadingCategories && <h3>Lista de Categorias:</h3>}
-
-        <ul>
-          {
+      <CategoriesList>
+        {
             categories && (
               categories.map((category) => (
-                <li key={category.id}>{category.name}</li>
+                <CategoryCard key={category.id}>
+                  <div className="info">
+                    <strong>{category.name}</strong>
+                  </div>
+
+                  <div className="actions">
+                    <Link to={`/editCategory/${category.id}`}>
+                      <img src={edit} alt="Edit" />
+                    </Link>
+
+                    <button
+                      onClick={() => handleDeleteCategory(category)}
+                      type="button"
+                    >
+                      <img src={trash} alt="Delete" />
+                    </button>
+                  </div>
+                </CategoryCard>
               ))
             )
           }
-        </ul>
-      </ListCategories>
+      </CategoriesList>
     </>
   );
 }
--- fe/src/pages/NewCategory/styles.js
+++ fe/src/pages/NewCategory/styles.js
@@ -1,20 +1,53 @@
 import styled from 'styled-components';
 
-export const ListCategories = styled.div``;
-
-export const CategoriesForm = styled.form``;
-
-export const ButtonContainer = styled.div`
-
-  margin: 24px 0px;
-
-  button {
-    width: 100%;
-  }
+export const TitleLabel = styled.h3`
+  margin-bottom: 16px;
 `;
 
-// router.get('/categories', CategoryController.index);
-// router.get('/categories/:id', CategoryController.show);
-// router.post('/categories', CategoryController.store);
-// router.put('/categories/:id', CategoryController.update);
-// router.delete('/categories/:id', CategoryController.delete);
+export const CategoriesList = styled.ul`
+  max-height: 400px;
+  overflow-y: scroll;
+
+  padding: 16px;
+  background: ${({ theme }) => theme.colors.gray[100]};
+  border-radius: 8px;
+
+
+  ::-webkit-scrollbar {
+    width: 0.6rem;
+}
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.gray[900]};
+    border-radius: 0.8rem;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.colors.gray[200]};
+  }
+
+`;
+
+export const CategoryCard = styled.div`
+  margin-bottom: 16px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  padding: 16px;
+  border-radius: 4px;
+
+  .actions {
+      display: flex;
+      align-items: center;
+
+      button {
+        background: transparent;
+        border: none;
+        margin-left: 8px;
+      }
+  }
+`;
