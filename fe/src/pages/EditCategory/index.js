b1b0b235da5402242a71992b90dee6a1bd32083d
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-06-21 13:50:42 -0300

    feat: Add Loader component

--- fe/src/pages/EditCategory/index.js
+++ fe/src/pages/EditCategory/index.js
@@ -1,6 +1,7 @@
 import PageHeader from '../../components/PageHeader';
 import CategoryForm from '../../components/CategoryForm';
 import useEditCategory from './useEditCategory';
+import Loader from '../../components/Loader';
 
 function EditCategory() {
   const {
@@ -12,7 +13,17 @@ function EditCategory() {
 
   return (
     <>
-      <PageHeader title={`Editar ${categoryFieldName}`} pathToReturn="/newCategory" />
+      <Loader isLoading={isLoading} />
+
+      <PageHeader
+        title={
+          isLoading
+            ? 'Carregando...'
+            : `Editar ${categoryFieldName}`
+        }
+        pathToReturn="/newCategory"
+      />
+
       <CategoryForm
         ref={categoryFormRef}
         buttonLabel="Salvar Alterações"
