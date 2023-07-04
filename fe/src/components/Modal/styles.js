51f579bee51dd510352541f0a1625d5e75df3da3
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-07-04 10:19:18 -0300

    refactor: add animation to modal with keyframes

--- fe/src/components/Modal/styles.js
+++ fe/src/components/Modal/styles.js
@@ -1,9 +1,28 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const scaleIn = keyframes`
+  from {
+    transform: scale(0);
+  }
+
+  to {
+    transform: scale(1);
+  }
+`;
 
 export const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.6);
   backdrop-filter: blur(3.5px);
-
   position: fixed;
   width: 100%;
   height: 100%;
@@ -12,6 +31,8 @@ export const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  animation: ${fadeIn} 0.3s;
 `;
 
 export const Container = styled.div`
@@ -21,6 +42,8 @@ export const Container = styled.div`
   border-radius: 4px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   padding: 24px;
+
+  animation: ${scaleIn} 0.3s;
 
   > h1 {
     font-size: 22px;
